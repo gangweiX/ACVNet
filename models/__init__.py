0e6d7d540b187f4f33183e60ed5ec6ffaeef4426
Gangwei Xu <34023118+gangweiX@users.noreply.github.com>
2022-03-20 11:50:24 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from models.acv import ACVNet
-from models.loss import model_loss_train, model_loss_test
+from models.loss import model_loss_train_attn_only, model_loss_train_freeze_attn, model_loss_train, model_loss_test
 
 __models__ = {
     "acvnet": ACVNet
