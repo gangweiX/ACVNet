051fcd08ac46a00a675112bd3fa79012ba76d655
Gangwei Xu <34023118+gangweiX@users.noreply.github.com>
2022-03-20 10:53:57 +0800

    second commit

--- models/loss.py
+++ models/loss.py
@@ -2,8 +2,22 @@ import torch.nn.functional as F
 import torch
 
 
+def model_loss_train_attn_only(disp_ests, disp_gt, mask):
+    weights = [1.0]
+    all_losses = []
+    for disp_est, weight in zip(disp_ests, weights):
+        all_losses.append(weight * F.smooth_l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
+    return sum(all_losses)
+
+def model_loss_train_freeze_attn(disp_ests, disp_gt, mask):
+    weights = [0.5, 0.7, 1.0]
+    all_losses = []
+    for disp_est, weight in zip(disp_ests, weights):
+        all_losses.append(weight * F.smooth_l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
+    return sum(all_losses)
+    
 def model_loss_train(disp_ests, disp_gt, mask):
-    weights = [0.7, 0.5, 0.7, 1.0]   #[0.5, 0.5, 0.7, 1.0]
+    weights = [0.5, 0.5, 0.7, 1.0] 
     all_losses = []
     for disp_est, weight in zip(disp_ests, weights):
         all_losses.append(weight * F.smooth_l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
@@ -13,5 +27,5 @@ def model_loss_test(disp_ests, disp_gt, mask):
     weights = [1.0] 
     all_losses = []
     for disp_est, weight in zip(disp_ests, weights):
-        all_losses.append(weight * F.smooth_l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
+        all_losses.append(weight * F.l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
     return sum(all_losses)
