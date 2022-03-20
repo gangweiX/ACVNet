dcbfa32364a278561bb2613c08d682022b189430
Gangwei Xu <34023118+gangweiX@users.noreply.github.com>
2022-03-20 10:52:35 +0800

    Update acv.py

--- models/acv.py
+++ models/acv.py
@@ -11,7 +11,7 @@ import time
 
 
 class feature_extraction(nn.Module):
-    def __init__(self, concat_feature_channel=32):
+    def __init__(self):
         super(feature_extraction, self).__init__()
         
         self.inplanes = 32
@@ -25,10 +25,6 @@ class feature_extraction(nn.Module):
         self.layer2 = self._make_layer(BasicBlock, 64, 16, 2, 1, 1)
         self.layer3 = self._make_layer(BasicBlock, 128, 3, 1, 1, 1)
         self.layer4 = self._make_layer(BasicBlock, 128, 3, 1, 1, 2)
-        self.lastconv = nn.Sequential(convbn(320, 128, 3, 1, 1, 1),
-                                          nn.ReLU(inplace=True),
-                                          nn.Conv2d(128, concat_feature_channel, kernel_size=1, padding=0, stride=1,
-                                                    bias=False))
 
     def _make_layer(self, block, planes, blocks, stride, pad, dilation):
         downsample = None
@@ -51,11 +47,9 @@ class feature_extraction(nn.Module):
         x = self.layer1(x)
         l2 = self.layer2(x)
         l3 = self.layer3(l2)
-
         l4 = self.layer4(l3)
         gwc_feature = torch.cat((l2, l3, l4), dim=1)
-        concat_feature = self.lastconv(gwc_feature)
-        return {"gwc_feature": gwc_feature, "concat_feature": concat_feature}
+        return {"gwc_feature": gwc_feature}
 
 
 class hourglass(nn.Module):
@@ -89,25 +83,28 @@ class hourglass(nn.Module):
     def forward(self, x):
         conv1 = self.conv1(x)
         conv2 = self.conv2(conv1)
-
         conv3 = self.conv3(conv2)
         conv4 = self.conv4(conv3)
         conv4 = self.attention_block(conv4)
-
-
         conv5 = F.relu(self.conv5(conv4) + self.redir2(conv2), inplace=True)
         conv6 = F.relu(self.conv6(conv5) + self.redir1(x), inplace=True)
-
         return conv6
 
-
 class ACVNet(nn.Module):
-    def __init__(self, maxdisp):
+    def __init__(self, maxdisp, attn_weights_only, freeze_attn_weights):
         super(ACVNet, self).__init__()
         self.maxdisp = maxdisp
+        self.attn_weights_only = attn_weights_only
+        self.freeze_attn_weights = freeze_attn_weights
         self.num_groups = 40
         self.concat_channels = 32
-        self.feature_extraction = feature_extraction(concat_feature_channel=self.concat_channels)
+        self.feature_extraction = feature_extraction()
+        self.concatconv = nn.Sequential(convbn(320, 128, 3, 1, 1, 1),
+                                          nn.ReLU(inplace=True),
+                                          nn.Conv2d(128, self.concat_channels, kernel_size=1, padding=0, stride=1,
+                                                    bias=False))
+
+        self.patch = nn.Conv3d(40, 40, kernel_size=(1,3,3), stride=1, dilation=1, groups=40, padding=(0,1,1), bias=False)
         self.patch_l1 = nn.Conv3d(8, 8, kernel_size=(1,3,3), stride=1, dilation=1, groups=8, padding=(0,1,1), bias=False)
         self.patch_l2 = nn.Conv3d(16, 16, kernel_size=(1,3,3), stride=1, dilation=2, groups=16, padding=(0,2,2), bias=False)
         self.patch_l3 = nn.Conv3d(16, 16, kernel_size=(1,3,3), stride=1, dilation=3, groups=16, padding=(0,3,3), bias=False)
@@ -120,7 +117,6 @@ class ACVNet(nn.Module):
                                       nn.ReLU(inplace=True),
                                       nn.Conv3d(16, 1, kernel_size=3, padding=1, stride=1, bias=False))
 
-
         self.dres0 = nn.Sequential(convbn_3d(self.concat_channels * 2, 32, 3, 1, 1),
                                    nn.ReLU(inplace=True),
                                    convbn_3d(32, 32, 3, 1, 1),
@@ -128,7 +124,6 @@ class ACVNet(nn.Module):
         self.dres1 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
                                    nn.ReLU(inplace=True),
                                    convbn_3d(32, 32, 3, 1, 1))        
-
         self.dres2 = hourglass(32)
         self.dres3 = hourglass(32)
         self.classif0 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
@@ -160,72 +155,96 @@ class ACVNet(nn.Module):
                 m.bias.data.zero_()
 
     def forward(self, left, right):
-        features_left = self.feature_extraction(left)
-        features_right = self.feature_extraction(right)
 
-        gwc_volume = build_gwc_volume(features_left["gwc_feature"], features_right["gwc_feature"], self.maxdisp // 4,
-                                      self.num_groups)
-        patch_l1 = self.patch_l1(gwc_volume[:, :8])
-        patch_l2 = self.patch_l2(gwc_volume[:, 8:24])
-        patch_l3 = self.patch_l3(gwc_volume[:, 24:40])
-        patch_volume = torch.cat((patch_l1,patch_l2,patch_l3), dim=1)
-        concat_volume = build_concat_volume(features_left["concat_feature"], features_right["concat_feature"], self.maxdisp // 4)
-        cost_attention = self.dres1_att(patch_volume)
-        cost_attention = self.dres2_att(cost_attention)
-        att_weights = self.classif_att(cost_attention)
-        ac_volume = att_weights * concat_volume
-
-        cost0 = self.dres0(ac_volume)
-        cost0 = self.dres1(cost0) + cost0
-        out1 = self.dres2(cost0)
-        out2 = self.dres3(out1)
-
-        if self.training:
-
-            cost0 = self.classif0(cost0)
-            cost1 = self.classif1(out1)
-            cost2 = self.classif2(out2)
-
-            cost_attention = F.upsample(att_weights, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
-            cost_attention = torch.squeeze(cost_attention, 1)
-            pred_attention = F.softmax(cost_attention, dim=1)
-            pred_attention = disparity_regression(pred_attention, self.maxdisp)
-
-            cost0 = F.upsample(cost0, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
-            cost0 = torch.squeeze(cost0, 1)
-            pred0 = F.softmax(cost0, dim=1)
-            pred0 = disparity_regression(pred0, self.maxdisp)
-
-            cost1 = F.upsample(cost1, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
-            cost1 = torch.squeeze(cost1, 1)
-            pred1 = F.softmax(cost1, dim=1)
-            pred1 = disparity_regression(pred1, self.maxdisp)
-
-            cost2 = F.upsample(cost2, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
-            cost2 = torch.squeeze(cost2, 1)
-            pred2 = F.softmax(cost2, dim=1)
-            pred2 = disparity_regression(pred2, self.maxdisp)
-
-            return [pred_attention, pred0, pred1, pred2]
+        if self.freeze_attn_weights:
+            with torch.no_grad():
+                features_left = self.feature_extraction(left)
+                features_right = self.feature_extraction(right)
+                gwc_volume = build_gwc_volume(features_left["gwc_feature"], features_right["gwc_feature"], self.maxdisp // 4, self.num_groups)
+                gwc_volume = self.patch(gwc_volume)
+                patch_l1 = self.patch_l1(gwc_volume[:, :8])
+                patch_l2 = self.patch_l2(gwc_volume[:, 8:24])
+                patch_l3 = self.patch_l3(gwc_volume[:, 24:40])
+                patch_volume = torch.cat((patch_l1,patch_l2,patch_l3), dim=1)
+                cost_attention = self.dres1_att(patch_volume)
+                cost_attention = self.dres2_att(cost_attention)
+                att_weights = self.classif_att(cost_attention)
 
         else:
 
-            # cost_attention = F.upsample(att_weights, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
-            # cost_attention = torch.squeeze(cost_attention, 1)
-            # pred_attention = F.softmax(cost_attention, dim=1)
-            # pred_attention = disparity_regression(pred_attention, self.maxdisp)
+            features_left = self.feature_extraction(left)
+            features_right = self.feature_extraction(right)
+            gwc_volume = build_gwc_volume(features_left["gwc_feature"], features_right["gwc_feature"], self.maxdisp // 4, self.num_groups)
+            gwc_volume = self.patch(gwc_volume)
+            patch_l1 = self.patch_l1(gwc_volume[:, :8])
+            patch_l2 = self.patch_l2(gwc_volume[:, 8:24])
+            patch_l3 = self.patch_l3(gwc_volume[:, 24:40])
+            patch_volume = torch.cat((patch_l1,patch_l2,patch_l3), dim=1)
+            cost_attention = self.dres1_att(patch_volume)
+            cost_attention = self.dres2_att(cost_attention)
+            att_weights = self.classif_att(cost_attention)
+
+        if not self.attn_weights_only:
+            concat_feature_left = self.concatconv(features_left["gwc_feature"])
+            concat_feature_right = self.concatconv(features_right["gwc_feature"])  
+            concat_volume = build_concat_volume(concat_feature_left, concat_feature_right, self.maxdisp // 4)
+            ac_volume = F.softmax(att_weights, dim=2) * concat_volume   ### ac_volume = att_weights * concat_volume 
+            cost0 = self.dres0(ac_volume)
+            cost0 = self.dres1(cost0) + cost0
+            out1 = self.dres2(cost0)
+            out2 = self.dres3(out1)
+
+        if self.training:
+
+            if not self.freeze_attn_weights:
+
+                cost_attention = F.upsample(att_weights, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost_attention = torch.squeeze(cost_attention, 1)
+                pred_attention = F.softmax(cost_attention, dim=1)
+                pred_attention = disparity_regression(pred_attention, self.maxdisp)
+
+            if not self.attn_weights_only:
+
+                cost0 = self.classif0(cost0)
+                cost1 = self.classif1(out1)
+                cost2 = self.classif2(out2)    
+
+                cost0 = F.upsample(cost0, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost0 = torch.squeeze(cost0, 1)
+                pred0 = F.softmax(cost0, dim=1)
+                pred0 = disparity_regression(pred0, self.maxdisp)    
+
+                cost1 = F.upsample(cost1, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost1 = torch.squeeze(cost1, 1)
+                pred1 = F.softmax(cost1, dim=1)
+                pred1 = disparity_regression(pred1, self.maxdisp)    
+
+                cost2 = F.upsample(cost2, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost2 = torch.squeeze(cost2, 1)
+                pred2 = F.softmax(cost2, dim=1)
+                pred2 = disparity_regression(pred2, self.maxdisp)
+
+                if self.freeze_attn_weights:
+                    return [pred0, pred1, pred2]
+                return [pred_attention, pred0, pred1, pred2]
+            return [pred_attention]
+
+        else:
+
+            if self.attn_weights_only:
+
+                cost_attention = F.upsample(att_weights, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
+                cost_attention = torch.squeeze(cost_attention, 1)
+                pred_attention = F.softmax(cost_attention, dim=1)
+                pred_attention = disparity_regression(pred_attention, self.maxdisp)
+                return [pred_attention]
 
             cost2 = self.classif2(out2)
             cost2 = F.upsample(cost2, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear')
             cost2 = torch.squeeze(cost2, 1)
             pred2 = F.softmax(cost2, dim=1)
             pred2 = disparity_regression(pred2, self.maxdisp)
-
-            # return [pred_attention, pred2]
             return [pred2]
-
-
-
 
 def acv(d):
     return ACVNet(d)
